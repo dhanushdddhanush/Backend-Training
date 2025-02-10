fdb61dda6b9eda6664e8cd65860232a09ca5bdad
Dhanush <98016619+dhanushdddhanush@users.noreply.github.com>
2025-02-10 13:57:04 +0530

    Update Day4Fibanacci.java

--- demo/Day4Fibanacci.java
+++ demo/Day4Fibanacci.java
@@ -1,5 +1,35 @@
-package demo;
+import java.util.Scanner;
 
 public class Day4Fibanacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
 
+        int num1 = 0, num2 = 1;
+
+        for (int i = 1; i <= num; i++) {
+            System.out.print(num1 + " ");
+
+            if (num1 % 3 == 0 && num1 % 5 == 0) {
+                System.out.println("I am spiderman and ironman");
+            } else if (num1 % 3 == 0) {
+                System.out.println("I am spiderman");
+            } else if (num1 % 5 == 0) {
+                System.out.println("I am ironman");
+            } else {
+                System.out.println("I am zero");
+            }
+
+            int next = num1 + num2;
+            num1 = num2;
+            num2 = next;
+
+           
+            if (num1 > num) {
+                break;
+            }
+        }
+    }
 }
